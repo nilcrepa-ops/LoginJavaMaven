c3897b49aee4d71b987a93b9a4df04a974f196c6
nilcrepa-ops <nilcrepa@gmail.com>
2026-02-27 12:10:20 +0100

    ya funciona

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.creus.login.login_register_creus {
     requires javafx.fxml;
     requires java.base;
 
-    opens com.creus.login.login_register_creus to javafx.fxml;
+    opens com.creus.login.login_register_creus.controller to javafx.fxml;
     exports com.creus.login.login_register_creus;
 }
